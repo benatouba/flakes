52bf6ea381fe130a4363288480f82489cdc8e9cb
Benjamin Schmidt <benschmidt@live.de>
2023-02-19 19:59:37 +0100

    mv networking to hardware config and README

--- hosts/system.nix
+++ hosts/system.nix
@@ -3,16 +3,6 @@
 {
   nixpkgs.system = "x86_64-linux";
 
-  networking = {
-    hostName = "bens-home"; # Define your hostname.
-    networkmanager.enable = true;
-    hosts = {
-      "185.199.109.133" = [ "raw.githubusercontent.com" ];
-      "185.199.111.133" = [ "raw.githubusercontent.com" ];
-      "185.199.110.133" = [ "raw.githubusercontent.com" ];
-      "185.199.108.133" = [ "raw.githubusercontent.com" ];
-    };
-  };
   time.timeZone = "Europe/Berlin";
 
   i18n.defaultLocale = "en_US.UTF-8";
