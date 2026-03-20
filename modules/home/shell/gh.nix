e490f209b9a8299b6317d56132362417584205da
Benjamin Schmidt <benschmidt@live.de>
2026-03-20 09:48:07 +0100

    feat(tool): ✨ add dark reader to brave

---
+++ modules/home/shell/gh.nix
@@ -0,0 +1,24 @@
+_:
+{
+  programs.gh = {
+    enable = true;
+    settings = {
+      git_protocol = "ssh";
+      editor = "nvim";
+      prompt = "enabled";
+      aliases = {
+        co = "pr checkout";
+        pv = "pr view";
+        pl = "pr list";
+        pc = "pr create";
+        rv = "repo view";
+        rc = "repo clone";
+        il = "issue list";
+        iv = "issue view";
+        ic = "issue create";
+      };
+    };
+  };
+
+  programs.gh-dash.enable = true;
+}
