c4ffef70c0110f10275b9658e7d8178220cb6f27
Benjamin Schmidt <benschmidt@live.de>
2026-03-24 00:51:34 +0100

    feat(flake-parts): ✨ add flake-parts and noctalia option

--- flake.nix
+++ flake.nix
@@ -1,57 +1,78 @@
 {
   description = "My flakes configuration";
 
-  inputs =
-    {
-      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-      hyprpicker.url = "github:hyprwm/hyprpicker";
-      hypr-contrib.url = "github:hyprwm/contrib";
-      neovim-nightly = {
-        url = "github:nix-community/neovim-nightly-overlay";
-        inputs.nixpkgs.follows = "nixpkgs";
-      };
-      hyprland = {
-        url = "github:hyprwm/Hyprland";
-        inputs.nixpkgs.follows = "nixpkgs";
-      };
-      home-manager = {
-        url = "github:nix-community/home-manager";
-        inputs.nixpkgs.follows = "nixpkgs";
-      };
-      impermanence.url = "github:nix-community/impermanence";
-      nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-      sops-nix = {
-        url = "github:Mic92/sops-nix";
-        inputs.nixpkgs.follows = "nixpkgs";
-      };
-      nix-secrets = {
-        url = "git+file:///home/ben/.local/secrets";
-        flake = false;
-      };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
     };
 
-  outputs = inputs @ { self, nixpkgs, ... }:
-    let
-      system = "x86_64-linux";
-      user = "ben";
-      themeName = "catppuccin-mocha"; # or "catppuccin-latte"
-      theme = import ./lib/theme.nix { inherit themeName; };
-      selfPkgs = import ./pkgs;
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [
-          self.overlays.default
-          inputs.neovim-nightly.overlays.default
-        ];
-      };
-    in
-    {
-      devShells.${system}.default = import ./shell.nix { inherit pkgs; };
+    hyprpicker.url = "github:hyprwm/hyprpicker";
+    hypr-contrib.url = "github:hyprwm/contrib";
+    neovim-nightly = {
+      url = "github:nix-community/neovim-nightly-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hyprland = {
+      url = "github:hyprwm/Hyprland";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    impermanence.url = "github:nix-community/impermanence";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    noctalia = {
+      url = "github:noctalia-dev/noctalia-shell";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.noctalia-qs.follows = "noctalia-qs";
+    };
+    noctalia-qs = {
+      url = "github:noctalia-dev/noctalia-qs";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-secrets = {
+      url = "git+file:///home/ben/.local/secrets";
+      flake = false;
+    };
+  };
 
-      overlays.default = selfPkgs.overlay;
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
 
-      nixosConfigurations = import ./hosts {
-          inherit system nixpkgs self inputs user theme;
+      flake = let
+        user = "ben";
+        themeName = "catppuccin-mocha"; # or "catppuccin-latte"
+        theme = import ./lib/theme.nix { inherit themeName; };
+      in {
+        overlays.default = (import ./pkgs).overlay;
+
+        nixosConfigurations = import ./hosts {
+          inherit user theme;
+          inherit (inputs) self nixpkgs;
+          inherit inputs;
+          system = "x86_64-linux";
         };
+      };
+
+      perSystem = { pkgs, system, ... }: {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [
+            inputs.self.overlays.default
+            inputs.neovim-nightly.overlays.default
+          ];
+        };
+
+        devShells.default = import ./shell.nix { inherit pkgs; };
+      };
     };
 }
