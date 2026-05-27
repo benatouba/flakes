6e626375b0fd0d6820d190d680577182bad500c5
Benjamin Schmidt <benschmidt@live.de>
2026-05-27 19:35:04 +0200

    feat(shell): ✨ prefer PNPM in devenv direnv sessions

--- cells/shell/zsh.nix
+++ cells/shell/zsh.nix
@@ -87,8 +87,18 @@ _: {
               # direnv (optimized): refresh on startup + directory changes only.
               # This avoids running `direnv export zsh` before every prompt.
               if (( $+commands[direnv] )); then
+                _prefer_pnpm_home_in_devenv() {
+                  if [[ -n "$DEVENV_ROOT" && -n "$PNPM_HOME" && -x "$PNPM_HOME/pnpm" ]]; then
+                    typeset -a _path_entries
+                    _path_entries=(''${(s/:/)PATH})
+                    _path_entries=(''${_path_entries:#$PNPM_HOME})
+                    export PATH="$PNPM_HOME:''${(j/:/)_path_entries}"
+                  fi
+                }
+
                 _direnv_chpwd_hook() {
                   eval "$(direnv export zsh)"
+                  _prefer_pnpm_home_in_devenv
                 }
 
                 autoload -U add-zsh-hook
