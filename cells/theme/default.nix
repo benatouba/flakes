01d7937971d398ef97ff7c22c744a4037787f9a5
Benjamin Schmidt <benschmidt@live.de>
2026-07-17 15:48:49 +0200

    fix(git): 🐛 dont commit auto-generated files
    and fix one deprection warning with the theme

--- cells/theme/default.nix
+++ cells/theme/default.nix
@@ -19,6 +19,7 @@ in
             inherit (theme.cursor) name;
             inherit (theme.cursor) size;
             gtk.enable = true;
+            enable = true;
           };
         };
         dconf.settings = {
